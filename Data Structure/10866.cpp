9c49c1cccc6aec2c182701f11d321b3cb97fd952
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2022-06-06 16:26:35 +0900

    [#1] 10866 - 덱

---
+++ Data Structure/10866.cpp
@@ -0,0 +1,51 @@
+//
+// Created by 이경민 on 2022/06/06.
+//
+#include<iostream>
+#include<string>
+#include<deque>
+#include<algorithm>
+using namespace std;
+
+string cmd;
+int n,x;
+deque<int> dq;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin>>n;
+    while(n--){
+        cin>>cmd;
+        if (cmd == "push_front") {
+            cin>>x;
+            dq.push_front(x);
+        }
+        else if (cmd == "push_back") {
+            cin>>x;
+            dq.push_back(x);
+        }
+        else if (cmd == "pop_front") {
+            if(dq.empty()) { cout<<"-1\n"; }
+            else { cout<<dq.front()<<"\n"; dq.pop_front(); }
+        }
+        else if (cmd == "pop_back") {
+            if(dq.empty()) { cout<<"-1\n"; }
+            else { cout<<dq.back()<<"\n"; dq.pop_back(); }
+        }
+        else if (cmd == "size") {
+            cout<<dq.size()<<"\n";
+        }
+        else if (cmd == "empty") {
+            cout<<dq.empty()<<"\n";
+        }
+        else if (cmd == "front") {
+            dq.empty() ? cout<<"-1\n" : cout<<dq.front()<<"\n";
+        }
+        else if (cmd == "back") {
+            dq.empty() ? cout<<"-1\n" : cout<<dq.back()<<"\n";
+        }
+    }
+}

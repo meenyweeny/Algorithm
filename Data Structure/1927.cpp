7e10f095bd18735e483e66be9d3d7c6c3d85d708
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2022-06-15 13:28:25 +0900

    [#1] 1927 - 최소 힙

---
+++ Data Structure/1927.cpp
@@ -0,0 +1,30 @@
+//
+// Created by 이경민 on 2022-06-15.
+//
+#include<iostream>
+#include<queue>
+using namespace std;
+
+int n,x;
+priority_queue<int, vector<int>, greater<int>> pq;
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    cin>>n;
+    while(n--){
+        cin>>x;
+        if(x==0) {
+            if (pq.empty()) cout<<"0\n";
+            else {
+                cout<<pq.top()<<"\n";
+                pq.pop();
+            }
+        }
+        else {
+            pq.push(x);
+        }
+    }
+}

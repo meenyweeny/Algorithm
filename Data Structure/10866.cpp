1abdac5747cb7a6eb246aa7bc0654eab05279213
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2022-08-26 03:37:31 +0900

    [#1] 10866 - 덱
    왜 전 커밋도 그렇고 결과가 이랬다 저랬다 나올까

--- Data Structure/10866.cpp
+++ Data Structure/10866.cpp
@@ -1,15 +1,13 @@
 //
-// Created by 이경민 on 2022/06/06.
+// Created by 이경민 on 2022/08/26.
 //
 #include<iostream>
-#include<string>
 #include<deque>
-#include<algorithm>
 using namespace std;
 
-string cmd;
-int n,x;
 deque<int> dq;
+int n,x;
+string cmd;
 
 int main() {
     cin.tie(NULL);
@@ -17,35 +15,44 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     cin>>n;
-    while(n--){
+    while(n--) {
         cin>>cmd;
-        if (cmd == "push_front") {
-            cin>>x;
-            dq.push_front(x);
-        }
-        else if (cmd == "push_back") {
+        if(cmd=="push_back") {
             cin>>x;
             dq.push_back(x);
+            continue;
         }
-        else if (cmd == "pop_front") {
-            if(dq.empty()) { cout<<"-1\n"; }
-            else { cout<<dq.front()<<"\n"; dq.pop_front(); }
+        else if(cmd=="push_front") {
+            cin>>x;
+            dq.push_front(x);
+            continue;
         }
-        else if (cmd == "pop_back") {
-            if(dq.empty()) { cout<<"-1\n"; }
-            else { cout<<dq.back()<<"\n"; dq.pop_back(); }
+        else if(cmd=="pop_front") {
+            if(dq.empty()) cout<<-1;
+            else {
+                cout << dq.front();
+                dq.pop_front();
+            }
         }
-        else if (cmd == "size") {
-            cout<<dq.size()<<"\n";
+        else if(cmd=="pop_back") {
+            if(dq.empty()) cout<<-1;
+            else {
+                cout << dq.back();
+                dq.pop_back();
+            }
         }
-        else if (cmd == "empty") {
-            cout<<dq.empty()<<"\n";
+        else if(cmd=="size") {
+            cout<<dq.size();
         }
-        else if (cmd == "front") {
-            dq.empty() ? cout<<"-1\n" : cout<<dq.front()<<"\n";
+        else if(cmd=="empty") {
+            cout<<dq.empty();
         }
-        else if (cmd == "back") {
-            dq.empty() ? cout<<"-1\n" : cout<<dq.back()<<"\n";
+        else if(cmd=="front") {
+            dq.empty() ? cout<<-1 : cout<<dq.front();
         }
+        else if(cmd=="back") {
+            dq.empty() ? cout<<-1 : cout<<dq.back();
+        }
+        cout<<"\n";
     }
 }

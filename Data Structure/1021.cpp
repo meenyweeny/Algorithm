41d150f1edd06deed27dd34df2c5b222f297ef06
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2022-08-31 01:48:57 +0900

    [#1] 1021 - 회전하는 큐

--- Data Structure/1021.cpp
+++ Data Structure/1021.cpp
@@ -1,64 +1,50 @@
 //
-//  1021.cpp
-//  BOJ
+// Created by 이경민 on 2022/08/31.
 //
-//  Created by 이경민 on 2022/05/11.
-//
-
 #include<iostream>
 #include<deque>
 using namespace std;
 
-int n,m,k,answer;
+int n,m,k;
+int answer;
 deque<int> dq;
-
-void move_front() {
-    ++answer;
-    dq.push_front(dq.back());
-    dq.pop_back();
-}
-
-void move_back() {
-    ++answer;
-    dq.push_back(dq.front());
-    dq.pop_front();
-}
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    
+
     cin>>n>>m;
-    for(int i=1; i<=n; i++){
-        dq.push_back(i);
-    }
-    
+    for(int i=1; i<=n; i++) dq.push_back(i);
+
     while(m--){
         cin>>k;
-        if(dq.front()==k) { dq.pop_front(); }
+        if(dq.front()==k) dq.pop_front();
         else if(dq.back()==k) {
             dq.pop_back();
             ++answer;
         }
         else {
-            for(int i=0; i<dq.size(); i++) {
+            for(int i=0; i<dq.size(); i++){
                 if(dq[i]!=k) continue;
-                if(i<=dq.size()/2) {
+                if(i<=(dq.size()/2)) {
                     for(int j=0; j<i; j++) {
-                        move_back();
+                        dq.push_back(dq.front());
+                        dq.pop_front();
+                        ++answer;
                     }
                     dq.pop_front();
                 }
                 else {
                     for(int j=0; j<dq.size()-i; j++){
-                        move_front();
+                        dq.push_front(dq.back());
+                        dq.pop_back();
+                        ++answer;
                     }
                     dq.pop_front();
                 }
             }
         }
     }
-    
     cout<<answer;
 }

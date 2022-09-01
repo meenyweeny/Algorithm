dc560ac2af6efb7f66e8ff6eb4da36a57d28bdaa
meenyweeny <74659491+meenyweeny@users.noreply.github.com>
2022-09-01 17:41:21 +0900

    [#1] 11279 - 최대 힙

--- Data Structure/11279.cpp
+++ Data Structure/11279.cpp
@@ -1,29 +1,28 @@
 //
-//  11279.cpp
-//  BOJ
+// Created by 이경민 on 2022/09/01.
 //
-//  Created by 이경민 on 2022/05/11.
-//
-
 #include<iostream>
 #include<queue>
 using namespace std;
 
-priority_queue<int> pq;
 int n,x;
+priority_queue<int, vector<int>> pq;
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    
+
     cin>>n;
     while(n--){
         cin>>x;
-        if (x==0) {
-            if(pq.empty()) cout<<"0\n";
-            else { cout<<pq.top()<<"\n"; pq.pop();}
-            continue;
+        if(x==0){
+            if(pq.empty()){
+                cout<<"0\n";
+                continue;
+            }
+            cout<<pq.top()<<"\n";
+            pq.pop();
         }
         pq.push(x);
     }

b65105dee28602e4808a27da6702b3896ccbcc43
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2022-10-04 22:53:26 +0900

    [#1] 2178 - 미로 탐색

--- BOJ/2178.cpp
+++ BOJ/2178.cpp
@@ -1,61 +1,79 @@
 //
-//  2178.cpp
-//  BOJ
+// Created by 이경민 on 2022/10/04.
 //
-//  Created by 이경민 on 2022/02/05.
-//
-
 #include<iostream>
 #include<queue>
-#include<string>
 using namespace std;
 
 int n,m;
-string s;
-bool map[100][100];
-bool visited[100][100];
-int dx[4] = {0,0,-1,1};
-int dy[4] = {-1,1,0,0};
+string str;
+bool map[101][101];
+int dx[4] = {-1,0,0,1};
+int dy[4] = {0,-1,1,0};
+bool visited[101][101];
 
-int bfs(){
-    queue<pair<int,pair<int,int>>> q;
-    q.push({1,{0,0}});
-    visited[0][0] = true;
-    
-    while(!q.empty()){
-        int cnt = q.front().first;
-        int x = q.front().second.first;
-        int y = q.front().second.second;
+bool isInRange(int x,int y) {
+    return x>=1 && x<=n && y>=1 && y<=m;
+}
+
+int bfs() {
+    queue<pair<pair<int,int>,int>> q;
+    q.push({{1,1},1});
+    visited[1][1] = true;
+
+    int nx,ny;
+
+    while(!q.empty()) {
+
+        int front_x = q.front().first.first;
+        int front_y = q.front().first.second;
+        int count = q.front().second;
         q.pop();
-        
-        if (x==n-1 && y==m-1) return cnt;
-        
-        for(int i=0; i<4; i++){
-            int nx = x+dx[i];
-            int ny = y+dy[i];
-            if(!visited[nx][ny] && map[nx][ny]==1) {
-                if(nx>=0 && nx<n && ny>=0 &&ny<m){
-                    q.push({cnt+1,{nx,ny}});
-                    visited[nx][ny] = true;
-                }
+
+        if(front_x == n && front_y == m) {
+            return count;
+        }
+
+        for(int i=0; i<4; i++) {
+            nx = front_x + dx[i];
+            ny = front_y + dy[i];
+
+            if(!isInRange(nx,ny)) {
+                continue;
             }
+
+            if(!map[nx][ny]) {
+                continue;
+            }
+
+            if(visited[nx][ny]) {
+                continue;
+            }
+
+            visited[nx][ny] = true;
+
+            q.push({{nx,ny},count+1});
         }
     }
-    
-    return 0;
+
+    return -1;
 }
+
+
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
-    
+
     cin>>n>>m;
-    for(int i=0; i<n; i++){
-        cin>>s;
-        for(int k=0; k<m; k++){
-            map[i][k] = s[k] -'0';
+
+    for(int i=1; i<=n; i++) {
+        cin>>str;
+        for(int k=1; k<=m; k++) {
+            map[i][k] = (str[k-1] == '1');
         }
     }
+
     cout<<bfs();
 }

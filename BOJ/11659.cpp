36cb47db7c18eaef59486edb5d324bd40de9e6e0
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-01-19 09:26:52 +0900

    [#4] 11659 - 구간 합 구하기 4

--- BOJ/11659.cpp
+++ BOJ/11659.cpp
@@ -1,23 +1,27 @@
 //
-// Created by 이경민 on 2022/06/16.
+// Created by 이경민 on 2023/01/19.
 //
 #include<iostream>
 using namespace std;
 
-int arr[100001];
-int sum[100001];
-int n,m,s,e;
+int n,m;
+int dp[100001];
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
+
     cin>>n>>m;
-    for(int i=0; i<n; i++) cin>>arr[i];
-    sum[0]=arr[0];
-    for(int i=1; i<n; i++) sum[i]=sum[i-1]+arr[i];
-    while(m--){
-        cin>>s>>e;
-        cout<<sum[e-1]-sum[s-2]<<"\n";
+    for(int i=1; i<=n; i++) {
+        cin>>dp[i];
+    }
+    for(int i=2; i<=n; i++) {
+        dp[i] += dp[i-1];
+    }
+    int i,j;
+    while(m--) {
+        cin>>i>>j;
+        cout<<dp[j]-dp[i-1]<<'\n';
     }
 }

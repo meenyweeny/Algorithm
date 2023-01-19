090b8e6975321577a7610009b4e60cd5506e3a02
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-01-19 10:36:10 +0900

    [#4] 2579 - 계단 오르기
    난 이 문제가 왜 이렇게 어렵지

--- BOJ/2579.cpp
+++ BOJ/2579.cpp
@@ -1,44 +1,38 @@
 //
-//  2579.cpp
-//  BOJ
+// Created by 이경민 on 2023/01/19.
 //
-//  Created by 이경민 on 2022/03/30.
-//
-
 #include<iostream>
-#include<string>
-#include<cmath>
 #include<algorithm>
-#include<queue>
-#include<vector>
-#include<cstdlib>
-#include<functional>
-#include<set>
-#include<stack>
 using namespace std;
 
-int stair[301];
-int score[301];
-
 int n;
+int dp[301];
+int stair[301];
 
-int main(){
-    cin.tie(NULL);
-    ios_base::sync_with_stdio(false);
-    
+void input() {
     cin>>n;
-    for(int i=1; i<=n; i++)
+    for(int i=1; i<=n; i++) {
         cin>>stair[i];
-    
-    score[1]=stair[1];
-    score[2]=stair[1]+stair[2];
-    score[3]=max(stair[1],stair[2])+stair[3];
-    
-    for(int i=4; i<=n; i++){
-        score[i]=max(score[i-2],score[i-3]+stair[i-1])+stair[i];
     }
-    
-    cout<<score[n];
 }
 
+void solution() {
+    dp[1] = stair[1];
+    dp[2] = dp[1] + stair[2];
+    dp[3] = max(stair[1], stair[2]) + stair[3];
 
+    for(int i=4; i<=n; i++) {
+        dp[i] = (max(dp[i-2], dp[i-3]+stair[i-1]) + stair[i]);
+    }
+
+    cout<<dp[n];
+}
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    input();
+    solution();
+}

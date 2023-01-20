f6d712f9ed891087afeac8e127148e9d539178c7
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-01-20 13:52:25 +0900

    [#4] 2504 - 괄호의 값
    어렵다..

--- BOJ/2504.cpp
+++ BOJ/2504.cpp
@@ -1,55 +1,64 @@
-//
-// Created by 이경민 on 2022/08/17.
-//
 #include<iostream>
 #include<stack>
 using namespace std;
 
 string cmd;
-stack<bool> st;
+stack<char> st;
 int answer;
-int tmp = 1;
-char ch;
+
+
+void input() {
+    cin>>cmd;
+}
+
+void solution() {
+    int sum = 1;
+
+    int length = cmd.length();
+    for(int i=0; i<length; i++) {
+        char now = cmd[i];
+        if(now == '(') {
+            st.push(now);
+            sum *= 2;
+        } else if(now == '[') {
+            st.push(now);
+            sum *= 3;
+        } else if(now == ']') {
+            if(st.empty() || st.top() != '[') {
+                cout<<0;
+                return;
+            } else {
+                if(cmd[i-1] == '[') {
+                    answer += sum;
+                }
+                sum /= 3;
+                st.pop();
+            }
+        } else {
+            if(st.empty() || st.top() != '(') {
+                cout<<0;
+                return;
+            } else {
+                if(cmd[i-1] == '(') {
+                    answer += sum;
+                }
+                sum /= 2;
+                st.pop();
+            }
+        }
+    }
+    if(st.empty()) {
+        cout<<answer;
+    } else {
+        cout<<0;
+    }
+}
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin>>cmd;
-
-    int length = cmd.length();
-
-    for(int i=0; i<length; i++) {
-        ch = cmd[i];
-        if(ch=='('){
-            st.push(0);
-            tmp *= 2;
-        }
-        else if (ch==')') {
-            if(st.empty() || st.top()!=0) {
-                cout<<0;
-                return 0;
-            }
-            if (cmd[i-1] == '(') answer += tmp;
-            st.pop();
-            tmp/=2;
-        }
-        else if(ch=='['){
-            st.push(1);
-            tmp *= 3;
-        }
-        else {
-            if(st.empty() || st.top()!= 1) {
-                cout<<0;
-                return 0;
-            }
-            if(cmd[i-1] == '[') answer+=tmp;
-            st.pop();
-            tmp/=3;
-        }
-    }
-
-    if(st.empty()) cout<<answer;
-    else cout<<0;
+    input();
+    solution();
 }

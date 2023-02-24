75522cc7513d82f4fa2583983c6600106355973b
이경민 <74659491+meenyweeny@users.noreply.github.com>
2023-02-24 20:03:27 +0900

    [#1] 15655 - N과 M (6)

---
+++ BOJ/15655.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int n,m;
+int arr[10];
+
+void input() {
+    cin>>n>>m;
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+}
+
+void solution(vector<int> v, int start) {
+    if(v.size()==m) {
+        for(auto i:v) {
+            cout<<i<<' ';
+        }
+        cout<<'\n';
+        return;
+    }
+
+    for(int i=start+1; i<n; i++) {
+        v.push_back(arr[i]);
+        solution(v,i);
+        v.pop_back();
+    }
+}
+
+void solve() {
+    input();
+    sort(arr,arr+n);
+    vector<int> vv;
+    solution(vv,-1);
+}
+
+int main() {
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
+
+    solve();
+}

b6ca8fb35b434ef262de8473e404e6c217cfe332
이경민 <74659491+meenyweeny@users.noreply.github.com>
2023-05-23 00:48:55 +0900

    [#1] 7576 - 토마토

--- BOJ/7576.cpp
+++ BOJ/7576.cpp
@@ -1,61 +1,77 @@
-//
-// Created by 이경민 on 2022/10/05.
-//
 #include<iostream>
 #include<queue>
-#include<algorithm>
 using namespace std;
 
-int n,m,x;
-queue<pair<pair<int,int>,int>> q;
-bool visited[1001][1001];
+struct Point {
+    int x,y,count;
+};
+
+int m,n;
 int map[1001][1001];
+bool visited[1001][1001];
 
-int dx[4] = {-1,0,0,1};
-int dy[4] = {0,-1,1,0};
+void input() {
+    cin>>m>>n;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            cin>>map[i][j];
+        }
+    }
+}
 
-bool is_in_range(int a,int b) {
-    return a>=1 && a<=n && b>=1 && b<=m;
+bool is_in_range(int x,int y) {
+    return x>=1 && y>=1 && x<=n && y<=m;
 }
 
 int bfs() {
-    int answer = -1;
-
+    queue<Point> q;
+    int count=0;
+    for(int i=1; i<=n; i++) {
+        for(int j=1; j<=m; j++) {
+            if(map[i][j]==1) {
+                visited[i][j] = true;
+                q.push({i,j,0});
+            }
+        }
+    }
     while(!q.empty()) {
-        int fx = q.front().first.first;
-        int fy = q.front().first.second;
-        int count = q.front().second;
+        Point front = q.front();
         q.pop();
-
-        answer = max(answer, count);
-
+        count = front.count;
         for(int i=0; i<4; i++) {
-            int nx = fx+dx[i];
-            int ny = fy+dy[i];
+            int nx = front.x + "2011"[i] - '1';
+            int ny = front.y + "1120"[i] - '1';
 
             if(!is_in_range(nx,ny)) {
                 continue;
             }
-            if(map[nx][ny]==-1) {
+            if(visited[nx][ny]) {
                 continue;
             }
-            if(map[nx][ny]==1) {
+            if(map[nx][ny]!=0) {
                 continue;
             }
             map[nx][ny] = 1;
-            q.push({{nx,ny},count+1});
+            visited[nx][ny] = true;
+            q.push({nx,ny,front.count+1});
         }
     }
+    return count;
+}
 
+void solution() {
+    int count = bfs();
     for(int i=1; i<=n; i++) {
-        for(int k=1; k<=m; k++) {
-            if(map[i][k]==0) {
-                return -1;
-            }
+        for(int j=1; j<=m; j++) {
+            count = (map[i][j] == 0) ? -1 : count;
         }
     }
+    cout<<count;
+}
 
-    return answer;
+void solve() {
+    input();
+    solution();
 }
 
 int main() {
@@ -63,17 +79,5 @@ int main() {
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin>>m>>n;
-
-    for(int i=1; i<=n; i++) {
-        for(int k=1; k<=m; k++) {
-            cin>>x;
-            map[i][k] = x;
-            if(x==1) {
-                q.push({{i,k},0});
-            }
-        }
-    }
-
-    cout<<bfs();
+    solve();
 }

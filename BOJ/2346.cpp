8b78cffb8dae7591f8ddf59e2b075df824ff757c
이경민 <74659491+meenyweeny@users.noreply.github.com>
2023-06-27 22:59:49 +0900

    파일 이동

--- BOJ/2346.cpp
+++ BOJ/2346.cpp
@@ -1,46 +1,54 @@
-//
-// Created by 이경민 on 2022/08/30.
-//
 #include<iostream>
 #include<deque>
 using namespace std;
 
-int n,x,nxt;
+int cnt;
 deque<pair<int,int>> dq;
+
+void input() {
+    int n,k;
+    cin>>n;
+    for(int i=1; i<=n; i++) {
+        cin>>k;
+        dq.push_back({i,k});
+    }
+}
+
+void turn_front(int count) {
+    for(int i=1; i<count; i++) {
+        dq.push_front(dq.back());
+        dq.pop_back();
+    }
+    cout<<dq.back().first<<' ';
+    cnt = dq.back().second;
+    dq.pop_back();
+}
+
+void turn_back(int count) {
+    for(int i=1; i<count; i++) {
+        dq.push_back(dq.front());
+        dq.pop_front();
+    }
+    cout<<dq.front().first<<' ';
+    cnt = dq.front().second;
+    dq.pop_front();
+}
+
+void solve() {
+    input();
+    while(!dq.empty()) {
+        if(cnt>=0) {
+            turn_back(cnt);
+        } else {
+            turn_front(-cnt);
+        }
+    }
+}
 
 int main() {
     cin.tie(NULL);
     cout.tie(NULL);
     ios_base::sync_with_stdio(false);
 
-    cin>>n;
-    for(int i=1; i<=n; i++) {
-        cin>>x;
-        dq.push_back({i,x});
-    }
-    nxt = dq.front().second;
-    cout<<dq.front().first<<" ";
-    dq.pop_front();
-
-    while(!dq.empty()){
-        if(nxt < 0) {
-            nxt *= -1;
-            for(int i=1; i < nxt; i++){
-                dq.push_front(dq.back());
-                dq.pop_back();
-            }
-            cout<<dq.back().first<<" ";
-            nxt = dq.back().second;
-            dq.pop_back();
-        }
-        else {
-            for (int i = 1; i < nxt; i++) {
-                dq.push_back(dq.front());
-                dq.pop_front();
-            }
-            cout<<dq.front().first<<" ";
-            nxt = dq.front().second;
-            dq.pop_front();
-        }
-    }
+    solve();
 }

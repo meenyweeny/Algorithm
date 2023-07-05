1769f7db0383a60882dd62b1c43ec6df922ba440
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-07-05 15:17:44 +0900

    [#1] 1158 - 요세푸스 문제

--- BOJ/1158.cpp
+++ BOJ/1158.cpp
@@ -1,33 +1,31 @@
-//
-// Created by 이경민 on 2022/08/22.
-//
 #include<iostream>
-#include<queue>
+#include<deque>
 using namespace std;
 
-int n,k;
-queue<int> q;
+int n, k;
+deque<int> dq;
+
+void solve() {
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		dq.push_back(i);
+	}
+	cout << '<';
+	while (dq.size() > 1) {
+		for (int i = 1; i < k; i++) {
+			dq.push_back(dq.front());
+			dq.pop_front();
+		}
+		cout << dq.front() << ", ";
+		dq.pop_front();
+	}
+	cout << dq.front() << '>';
+}
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin>>n>>k;
-    cout<<"<";
-    for(int i=1; i<=n; i++) {
-        q.push(i);
-    }
-
-    while(!q.empty()) {
-        for(int i=1; i<k; i++) {
-            q.push(q.front());
-            q.pop();
-        }
-        cout<<q.front();
-        q.pop();
-        if(!q.empty()) cout<<", ";
-    }
-
-    cout<<">";
+	solve();
 }

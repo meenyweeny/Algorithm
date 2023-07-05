9f566a11f46122c042012c7c85e292b7714dc808
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-07-05 16:13:46 +0900

    [#1] 13335 - 트럭

--- BOJ/13335.cpp
+++ BOJ/13335.cpp
@@ -1,51 +1,58 @@
-//
-// Created by 이경민 on 2022/08/24.
-//
 #include<iostream>
-#include<deque>
 #include<queue>
+#include<deque>
 using namespace std;
 
-#define weight first
-#define distance second
+int n, w, l;
+queue<int> car;
+deque<pair<int, int>> bridge;
+int weight, answer;
 
-int n,w,L,a;
-int now_weight,answer;
-queue<int> bus;
-deque<pair<int,int>> bridge; //무게, 위치
+void input() {
+	cin >> n >> w >> l;
+	int k;
+	for (int i = 0; i < n; i++) {
+		cin >> k;
+		car.push(k);
+	}
+}
+
+void process() {
+	for (int i = 0; i < bridge.size(); i++) {
+		bridge[i].second += 1;
+	}
+	if (bridge.front().second == w) {
+		weight -= bridge.front().first;
+		bridge.pop_front();
+	}
+	++answer;
+}
+
+
+void solution() {
+	while (!car.empty()) {
+		if (car.front() <= (l - weight)) {
+			bridge.push_back({ car.front(),0 });
+			weight += car.front();
+			car.pop();
+		}
+		process();
+	}
+	while (!bridge.empty()) {
+		process();
+	}
+	cout << answer+1;
+}
+
+void solve() {
+	input();
+	solution();
+}
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin >> n >> w >> L;
-    for(int i=0; i<n; i++) {
-        cin>>a;
-        bus.push(a);
-    }
-
-    while(!((bridge.empty() && bus.empty()))){
-        ++answer;
-
-        for(int i=0; i<bridge.size(); i++) {
-            ++bridge[i].distance;
-        }
-
-        if(!bridge.empty()) {
-            if(bridge[0].distance == w) {
-                now_weight -= bridge[0].weight;
-                bridge.pop_front();
-            }
-        }
-        if(!bus.empty()){
-            if(bus.front() + now_weight <= L) {
-                now_weight += bus.front();
-                bridge.push_back({bus.front(), 0});
-                bus.pop();
-            }
-        }
-    }
-
-    cout<<answer;
+	solve();
 }

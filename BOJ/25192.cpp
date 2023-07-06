4d8370adbaa8126e49bdbdb19eed71cdf113e6d3
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-07-06 11:09:58 +0900

    [#1] 25192 - 인사성 밝은 곰곰이

--- BOJ/25192.cpp
+++ BOJ/25192.cpp
@@ -1,31 +1,30 @@
 #include<iostream>
+#include<string>
 #include<set>
 using namespace std;
 
 set<string> s;
 
 void solve() {
-    int n;
-    cin>>n;
-    string cmd;
-    int answer = 0;
-    while(n--) {
-        cin>>cmd;
-        if(cmd=="ENTER") {
-            s.clear();
-        } else {
-            if(s.insert(cmd).second) {
-                ++answer;
-            }
-        }
-    }
-    cout<<answer;
+	int n;
+	cin >> n;
+	string cmd;
+	int answer = 0;
+	while (n--) {
+		cin >> cmd;
+		if (cmd == "ENTER") {
+			s.clear();
+		} else {
+			answer += s.insert(cmd).second;
+		}
+	}
+	cout << answer;
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+	ios_base::sync_with_stdio(false);
 
-    solve();
+	solve();
 }

83ca6bb98f3022caea570052f8a24f47e7439ce7
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-07-19 13:11:33 +0900

    [#1] 1806 - 부분합

--- BOJ/1806.cpp
+++ BOJ/1806.cpp
@@ -1,43 +1,38 @@
 #include<iostream>
 #include<algorithm>
-#include<climits>
 using namespace std;
 
-int arr[100001];
-int n,s,sum;
-int front, back;
-int answer = INT_MAX;
+const int sz = 1e5;
+int n, s, answer = 1e9;
+int arr[sz];
+
+void solve() {
+	cin >> n >> s;
+	for (int i = 1; i <= n; i++) {
+		cin >> arr[i];
+		arr[i] += arr[i - 1];
+	}
+	int now, length, front, end;
+	length = front = end = 1;
+	while (front <= n && end <= n) {
+		now = arr[end] - arr[front - 1];
+		if (now < s) {
+			++end;
+		} else {
+			answer = min(answer, end - front + 1);
+			++front;
+		}
+	}
+	if (answer > sz) {
+		answer = 0;
+	}
+	cout << answer;
+}
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin>>n>>s;
-    cin>>arr[0];
-    for(int i=1; i<n; i++) {
-        cin>>back;
-        arr[i] = arr[i-1] + back;
-    }
-    front=back=0;
-    while(front<=n-1 && back<=n-1) {
-        if(front>0) {
-            sum = arr[back] - arr[front-1];
-        } else {
-            sum = arr[back];
-        }
-
-        if(sum>=s) {
-            answer = min(answer, back-front+1);
-            ++front;
-        } else {
-            ++back;
-        }
-    }
-
-    if(answer != INT_MAX) {
-        cout<<answer;
-    } else {
-        cout<<0;
-    }
+	solve();
 }

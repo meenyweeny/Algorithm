0b3294fff32dc628fe5f2437d6acbe3cdba36683
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-07-20 11:42:21 +0900

    [#1] 10819 - 차이를 최대로

--- BOJ/10819.cpp
+++ BOJ/10819.cpp
@@ -1,45 +1,46 @@
-//
-// Created by 이경민 on 2022/08/07.
-//
 #include<iostream>
-#include<vector>
-#include<algorithm>
 #include<cmath>
+#include<algorithm>
 using namespace std;
 
-int n,answer;
+int n, gap = -2e9;
 int arr[8];
 bool visited[8];
-vector<int> v;
+
+void process(int count, int now[8]) {
+	if (count == n) {
+		int sum = 0;
+		for (int i = 0; i < n-1; i++) {
+			sum += abs(now[i] - now[i + 1]);
+		}
+		gap = max(gap, sum);
+        return;
+	}
+	for (int i = 0; i < n; i++) {
+		if (visited[i]) {
+			continue;
+		}
+		visited[i] = true;
+		now[count] = arr[i];
+		process(count + 1, now);
+		visited[i] = false;
+	}
+}
 
 void solve() {
-    if(v.size()==n) {
-        int sum=0;
-        for(int i=0; i<n-1; i++) {
-            sum += abs(v[i]-v[i+1]);
-        }
-        answer=max(answer,sum);
-        return;
-    }
-
-    for(int i=0; i<n; i++){
-        if(!visited[i]){
-            visited[i]=true;
-            v.push_back(arr[i]);
-            solve();
-            visited[i]=false;
-            v.pop_back();
-        }
-    }
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+	int tmp[8] = { 0, };
+	process(0, tmp);
+	cout << gap;
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin>>n;
-    for(int i=0; i<n; i++) cin>>arr[i];
-    solve();
-    cout<<answer;
+	solve();
 }

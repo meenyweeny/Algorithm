864a9f58f67a3d14b946786fdfafcf520cca8d2f
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-07-20 11:56:44 +0900

    [#1] 6603 - 로또

--- BOJ/6603.cpp
+++ BOJ/6603.cpp
@@ -1,60 +1,43 @@
-//
-// Created by 이경민 on 2023/02/02.
-//
 #include<iostream>
-#include<cstring>
 #include<vector>
 using namespace std;
 
 int k;
-bool visited[50];
-vector<int> vec;
+int arr[13];
+vector<int> lotto;
 
-void lotto(int count, int idx, bool visit[]) {
-    if(count == 6) {
-        for(int i=1; i<50; i++) {
-            if(visit[i]) {
-                cout<<i<<' ';
-            }
-        }
-        cout<<'\n';
-        return;
-    }
-    if(idx>=k) {
-        return;
-    }
-    for(int i=idx; i<k; i++) {
-        if(!visit[vec[i]]) {
-            visit[vec[i]] = true;
-            lotto(count+1, i+1, visit);
-            visit[vec[i]] = false;
-        }
-    }
+void process(int start) {
+	if (lotto.size() == 6) {
+		for (auto l : lotto) {
+			cout << l << ' ';
+		}
+		cout << '\n';
+		return;
+	}
+	for (int i = start; i < k; i++) {
+		lotto.push_back(arr[i]);
+		process(i + 1);
+		lotto.pop_back();
+	}
+}
+
+void solve() {
+	cin >> k;
+	while (k != 0) {
+		for (int i = 0; i < k; i++) {
+			cin >> arr[i];
+		}
+		lotto.clear();
+		process(0);
+		cout << '\n';
+		cin >> k;
+	}
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    int x;
-
-    while(1) {
-        cin>>k;
-        if(k==0) {
-            break;
-        }
-
-        vec.clear();
-        memset(visited,false,sizeof(visited));
-
-        for(int j=0; j<k; j++) {
-            cin>>x;
-            vec.push_back(x);
-        }
-
-        lotto(0, 0,visited);
-        cout<<'\n';
-    }
-
+	solve();
 }

d3e0ea2828075f498a935d6fc7541cbc5c65f4d6
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-01 10:41:25 +0900

    [#1] 24445 - 알고리즘 수업 - 너비 우선 탐색 2

---
+++ BOJ/24445.cpp
@@ -0,0 +1,53 @@
+#include<iostream>
+#include<deque>
+#include<algorithm>
+using namespace std;
+
+const int sz = 1e5 + 1;
+int n, m, r;
+deque<int> graph[sz];
+int turn[sz];
+int cnt = 1;
+
+void bfs() {
+	deque<int> dq;
+	dq.push_back(r);
+	turn[r] = cnt;
+	while (!dq.empty()) {
+		int front = dq.front();
+		dq.pop_front();
+		for (auto g : graph[front]) {
+			if (turn[g]) {
+				continue;
+			}
+			turn[g] = ++cnt;
+			dq.push_back(g);
+		}
+	}
+}
+
+void solve() {
+	cin >> n >> m >> r;
+	int u, v;
+	while (m--) {
+		cin >> u >> v;
+		graph[u].push_back(v);
+		graph[v].push_back(u);
+	}
+	for (int i = 1; i <= n; i++) {
+		sort(graph[i].begin(), graph[i].end());
+		reverse(graph[i].begin(), graph[i].end());
+	}
+	bfs();
+	for (int i = 1; i <= n; i++) {
+		cout << turn[i] << '\n';
+	}
+}
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
+}

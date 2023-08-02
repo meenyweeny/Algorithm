ac758d8582f4a57355455774a09764102324cfe2
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-02 12:07:43 +0900

    15649 - N과 M (1)

---
+++ JAVA/15649.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	static StringBuilder answer = new StringBuilder();
+	static int[] arr = new int[9];
+	static int r;
+	static int n;
+	static boolean visited[] = new boolean[9];
+
+	public static void s(int cnt) {
+		if(cnt==r) {
+			for(int i=0; i<r; i++) {
+				answer.append(arr[i]);
+				answer.append(' ');
+			}
+			answer.append('\n');
+			return;
+		}
+		for(int i=1; i<=n; i++) {
+			if(visited[i]) {
+				continue;
+			}
+			arr[cnt] = i;
+			visited[i] = true;
+			s(cnt+1);
+			visited[i] = false;
+		}
+	}
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		
+		n = Integer.parseInt(st.nextToken());
+		r = Integer.parseInt(st.nextToken());
+		
+		s(0);
+		System.out.println(answer);
+	}
+}

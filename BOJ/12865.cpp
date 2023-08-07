3d832d4843bcdc1eace451921ec67cc2c95f986c
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-07 15:22:05 +0900

    12865 - 평범한 배낭

--- BOJ/12865.cpp
+++ BOJ/12865.cpp
@@ -2,36 +2,40 @@
 #include<algorithm>
 using namespace std;
 
-int n,k;
-int weight[101];
-int value[101];
-int dp[101][100001];
+int n, k;
+int w[101];
+int v[101];
+int knapsack[101][100001];
 
 void input() {
-    cin>>n>>k;
-    for(int i=1; i<=n; i++) {
-        cin>>weight[i]>>value[i];
-    }
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) {
+		cin >> w[i] >> v[i];
+	}
 }
 
-void run() {
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=k; j++) {
-            if(j>=weight[i]) {
-                dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight[i]]+value[i]);
-            } else {
-                dp[i][j] = dp[i-1][j];
-            }
-        }
-    }
+void solution() {
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= k; j++) {
+			if (w[i] > j) {
+				knapsack[i][j] = knapsack[i - 1][j];
+			} else {
+				knapsack[i][j] = max(knapsack[i - 1][j], knapsack[i-1][j - w[i]] + v[i]);
+			}
+		}
+	}
+	cout << knapsack[n][k];
+}
+
+void solve() {
+	input();
+	solution();
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    input();
-    run();
-    cout<<dp[n][k];
+	solve();
 }

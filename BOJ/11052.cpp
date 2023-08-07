e73fa19469c6f30d31040bea39c17c72c485eb98
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-07 16:47:33 +0900

    [#1] 11052 - 카드 구매하기

--- BOJ/11052.cpp
+++ BOJ/11052.cpp
@@ -1,27 +1,39 @@
-//
-// Created by 이경민 on 2022/09/08.
-//
 #include<iostream>
 #include<algorithm>
 using namespace std;
 
+const int sz = 1e3 + 1;
 int n;
-int price[1001];
-int dp[1001];
+int p[sz];
+int dp[sz];
+
+void input() {
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		cin >> p[i];
+	}
+}
+
+void solution() {
+	dp[1] = p[1];
+	for (int i = 2; i <= n; i++) {
+		dp[i] = p[i];
+		for (int j = 1; j <= i/2; j++) {
+			dp[i] = max(dp[i - j] + dp[j], dp[i]);
+		}
+	}
+	cout << dp[n];
+}
+
+void solve() {
+	input();
+	solution();
+}
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin>>n;
-    for(int i=1; i<=n; i++) cin>>price[i];
-    dp[1]=price[1];
-    for(int i=2; i<=n; i++) {
-        dp[i]= price[i];
-        for(int k=i; k>=1; k--){
-            dp[i]=max(dp[i],dp[i-k]+dp[k]);
-        }
-    }
-    cout<<dp[n];
+	solve();
 }

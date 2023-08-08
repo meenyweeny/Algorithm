b52c40b7fa70feaeb3fb8fd3c8a3f1bb7b49cb0f
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-08 10:43:04 +0900

    [#1] 10844 - 쉬운 계단 수

--- BOJ/10844.cpp
+++ BOJ/10844.cpp
@@ -1,31 +1,39 @@
-//
-// Created by 이경민 on 2022/09/13.
-//
 #include<iostream>
 using namespace std;
 
-#define mod 1000000000
-
-int n,answer;
+const int mod = 1e9;
+int n;
 unsigned long long dp[101][10];
 
-int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+void solve() {
+	cin >> n;
+	for (int i = 1; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j < 9; j++) {
+			dp[i][j] += dp[i - 1][j - 1];
+			dp[i][j] %= mod;
+			dp[i][j] += dp[i - 1][j + 1];
+			dp[i][j] %= mod;
+		}
+		dp[i][9] += dp[i - 1][8];
+		dp[i][9] %= mod;
+		dp[i][0] += dp[i - 1][1];
+		dp[i][0] %= mod;
+	}
+	unsigned long long answer = 0;
+	for (int i = 0; i < 10; i++) {
+		answer += dp[n][i];
+		answer %= mod;
+	}
+	cout << answer;
+}
 
-    cin>>n;
-    for(int i=1; i<10; i++) dp[1][i]=1;
-    for(int i=2; i<=n; i++) {
-        dp[i][0]=dp[i-1][1]%mod;
-        for(int k=1; k<9; k++){
-            dp[i][k] = (dp[i-1][k-1] + dp[i-1][k+1])%mod;
-        }
-        dp[i][9] = (dp[i-1][8]);
-    }
-    for(int i=0; i<10; i++) {
-        answer += (dp[n][i]);
-        answer %= mod;
-    }
-    cout<<answer;
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
+
+	solve();
 }

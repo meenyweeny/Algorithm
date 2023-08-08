b3d9f160304504e1873f1e12f8c65f12de52efe5
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-08 11:07:14 +0900

    [#1] 1932 - 정수 삼각형

--- BOJ/1932.cpp
+++ BOJ/1932.cpp
@@ -1,41 +1,50 @@
-//
-// Created by 이경민 on 2023/01/19.
-//
 #include<iostream>
 #include<algorithm>
 using namespace std;
 
 int n;
+int tri[501][501];
 int dp[501][501];
 
 void input() {
-    cin>>n;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=i; j++) {
-            cin>>dp[i][j];
-        }
-    }
+	cin >> n;
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			cin >> tri[i][j];
+		}
+	}
 }
 
 void solution() {
-    for(int i=2; i<=n; i++) {
-        for(int j=1; j<=i; j++) {
-            dp[i][j] += max(dp[i-1][j-1], dp[i-1][j]);
-        }
-    }
+	dp[1][1] = tri[1][1];
+	dp[2][1] = (tri[2][1] + dp[1][1]);
+	dp[2][2] = (tri[2][2] + dp[1][1]);
+	
+	for (int i = 3; i <= n; i++) {
+		dp[i][1] = (tri[i][1] + dp[i - 1][1]);
+		for (int j = 2; j < i; j++) {
+			dp[i][j] += tri[i][j];
+			dp[i][j] += max(dp[i-1][j - 1], dp[i-1][j]);
+		}
+		dp[i][i] = (tri[i][i] + dp[i - 1][i - 1]);
+	}
 
-    int answer = -1;
-    for(int i=1; i<=n; i++) {
-        answer = max(dp[n][i], answer);
-    }
-    cout<<answer;
+	int answer = -1;
+	for (int i = 0; i <= n; i++) {
+		answer = max(answer, dp[n][i]);
+	}
+	cout << answer;
+}
+
+void solve() {
+	input();
+	solution();
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    input();
-    solution();
+	solve();
 }

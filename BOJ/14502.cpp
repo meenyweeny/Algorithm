f5bb2998c8031a23aee6c8aa1ea9e711ae4baae8
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-11 17:43:57 +0900

    [#1] 14502 - 연구소

--- BOJ/14502.cpp
+++ BOJ/14502.cpp
@@ -1,112 +1,104 @@
 #include<iostream>
-#include<vector>
-#include<queue>
+#include<deque>
 #include<algorithm>
 using namespace std;
 
-int n,m;
+const int bug = 2;
+const int wall = 1;
+int n, m, answer = -1;
 int map[8][8];
-int dx[4] = {-1,0,0,1};
-int dy[4] = {0,-1,1,0};
-vector<pair<int,int>> safe_area;
-vector<pair<int,int>> virus;
-int max_area = -1;
+deque<pair<int, int>> virus;
+deque<pair<int, int>> safearea;
+bool visited[8][8];
 
-bool is_in_range(int x,int y) {
-    return x>=0 && x<n && y>=0 && y<m;
+void init() {
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			visited[i][j] = (map[i][j] != 0);
+		}
+	}
+}
+
+int getSafeareaSize() {
+	int ret = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			ret += (!visited[i][j]);
+		}
+	}
+	return ret;
+}
+
+bool check_range(int x, int y) {
+	return x >= 0 && y >= 0 && x < n && y < m;
 }
 
 void bfs() {
-    queue<pair<int,int>> q;
+	deque<pair<int, int>> q = virus;
+	while (!q.empty()) {
+		int fx = q.front().first;
+		int fy = q.front().second;
+		q.pop_front();
+		visited[fx][fy] = true;
+		for (int i = 0; i < 4; i++) {
+			int nx = fx + "2011"[i] - '1';
+			int ny = fy + "1120"[i] - '1';
 
-    int tmp_map[8][8];
-
-    for(int i=0; i<n; i++) {
-        for(int k=0; k<m; k++) {
-            tmp_map[i][k] = map[i][k];
-        }
-    }
-
-    for(auto i:virus) {
-        q.push({i.first, i.second});
-    }
-
-    while(!q.empty()) {
-        int fx = q.front().first;
-        int fy = q.front().second;
-        q.pop();
-
-        for(int i=0; i<4; i++) {
-            int nx = fx + dx[i];
-            int ny = fy + dy[i];
-
-            if(!is_in_range(nx,ny)) {
-                continue;
-            }
-            if(map[nx][ny] == 1 || map[nx][ny] == 2) {
-                continue;
-            }
-
-            map[nx][ny] = 2;
-            q.push({nx,ny});
-        }
-    }
-
-    int count = 0;
-    for(int i=0; i<n; i++) {
-        for(int k=0; k<m; k++) {
-            if(map[i][k] == 0) {
-                ++count;
-            }
-        }
-    }
-
-    max_area = max(max_area, count);
-
-    for(int i=0; i<n; i++) {
-        for(int k=0; k<m; k++) {
-            map[i][k] = tmp_map[i][k];
-        }
-    }
+			if (!check_range(nx, ny)) {
+				continue;
+			}
+			if (visited[nx][ny]) {
+				continue;
+			}
+			if (map[nx][ny] != 0) {
+				continue;
+			}
+			visited[nx][ny] = true;
+			q.push_back({ nx,ny });
+		}
+	}
 }
 
-void dfs(int count, int start) {
-    if(count == 3) {
-        bfs();
-        return;
-    }
+void run(int count,int x) {
+	if (count == 3) {
+		init();
+		bfs();
+		answer = max(answer, getSafeareaSize());
+		return;
+	}
 
-    for(int i=start; i<safe_area.size(); i++) {
-        if(map[safe_area[i].first][safe_area[i].second] != 0) {
-            continue;
-        }
-        if(count==0 && start >= safe_area.size()-2) {
-            break;
-        }
-        map[safe_area[i].first][safe_area[i].second] = 1;
-        dfs(count+1, start+1);
-        map[safe_area[i].first][safe_area[i].second] = 0;
-    }
+	for (int i = x; i < safearea.size(); i++) {
+		pair<int, int> now = safearea[i];
+		if (map[now.first][now.second] != 0) {
+			continue;
+		}
+		map[now.first][now.second] = wall;
+		run(count + 1, i + 1);
+		map[now.first][now.second] = 0;
+	}
+}
+
+void solve() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> map[i][j];
+			if (map[i][j] == bug) {
+				virus.push_back({i,j});
+			}
+			else if (!map[i][j]) {
+				safearea.push_back({ i,j });
+			}
+		}
+	}
+	run(0, 0);
+	cout << answer;
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin>>n>>m;
-    for(int i=0; i<n; i++) {
-        for(int k=0; k<m; k++) {
-            cin>>map[i][k];
-            if(map[i][k] == 0) {
-                safe_area.push_back({i, k});
-            }
-            else if(map[i][k] == 2) {
-                virus.push_back({i,k});
-            }
-        }
-    }
-
-    dfs(0,0);
-    cout<<max_area;
+	solve();
 }

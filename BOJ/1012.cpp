0e8816294497c8455803053774302eb78a70ae36
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-11 09:28:19 +0900

    [#1] 1012 - 유기농 배추

--- BOJ/1012.cpp
+++ BOJ/1012.cpp
@@ -1,82 +1,71 @@
-//
-// Created by 이경민 on 2022/10/04.
-//
 #include<iostream>
-#include<cstring>
 #include<queue>
 using namespace std;
 
-int dx[4] = {-1,0,0,1};
-int dy[4] = {0,-1,1,0};
+int n, m;
 bool map[50][50];
-bool visited[50][50];
-int n,m,t,k,a,b,answer;
 
-bool is_in_range(int x,int y) {
-    return x>=0 && x<n && y>=0 && y<m;
+void init() {
+	fill(&map[0][0], &map[49][50], false);
+}
+
+bool check_range(int x, int y) {
+	return x >= 0 && y >= 0 && x < n && y < m;
 }
 
 void bfs(int x,int y) {
-    queue<pair<int,int>> q;
-    q.push({x,y});
-    visited[x][y] = true;
+	queue<pair<int, int>> q;
+	q.push({ x,y });
+	map[x][y] = false;
 
-    while(!q.empty()) {
-        x = q.front().first;
-        y = q.front().second;
-        q.pop();
+	while (!q.empty()) {
+		int fx = q.front().first;
+		int fy = q.front().second;
+		q.pop();
 
-        for(int i=0; i<4; i++) {
-            int nx = x + dx[i];
-            int ny = y + dy[i];
+		for (int i = 0; i < 4; i++) {
+			int nx = fx + "2011"[i] - '1';
+			int ny = fy + "1120"[i] - '1';
 
-            if(!is_in_range(nx,ny)) {
-                continue;
-            }
+			if (!check_range(nx, ny)) {
+				continue;
+			}
+			if (!map[nx][ny]) {
+				continue;
+			}
+			q.push({ nx,ny });
+			map[nx][ny] = false;
+		}
+	}
+}
 
-            if(visited[nx][ny]) {
-                continue;
-            }
-
-            if(!map[nx][ny]) {
-                continue;
-            }
-
-            visited[nx][ny] = true;
-            q.push({nx,ny});
-        }
-    }
+void solve() {
+	int t, k, x, y;
+	cin >> t;
+	while (t--) {
+		cin >> m >> n >> k;
+		while (k--) {
+			cin >> y >> x;
+			map[x][y] = true;
+		}
+		int answer = 0;
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < m; j++) {
+				if (map[i][j]) {
+					bfs(i, j);
+					++answer;
+				}
+			}
+		}
+		cout << answer << '\n';
+		init();
+	}
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    cin>>t;
-
-    while(t--) {
-
-        cin>>m>>n>>k;
-
-        while(k--) {
-            cin>>a>>b;
-            map[b][a]= true;
-        }
-
-        for(int i=0; i<n; i++) {
-            for(int j=0; j<m; j++) {
-                if(!visited[i][j] && map[i][j]) {
-                    ++answer;
-                    bfs(i,j);
-                }
-            }
-        }
-
-        cout<<answer<<"\n";
-
-        answer = 0;
-        memset(map,false,sizeof(map));
-        memset(visited,false,sizeof(visited));
-    }
+	solve();
 }

50d4268b7294686b4f06952973f4aacf8048678e
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-17 12:03:40 +0900

    [#1] 1753 - 최단경로

--- BOJ/1753.cpp
+++ BOJ/1753.cpp
@@ -1,81 +1,79 @@
 #include<iostream>
 #include<queue>
-#include<vector>
+#include<string>
 using namespace std;
 
 struct edge {
-    int vertex;
-    int weight;
+	int vertex;
+	int weight;
 
-    bool operator<(const edge& b) const {
-        return weight > b.weight;
-    }
+	bool operator<(const edge& b) const {
+		return this->weight > b.weight;
+	}
 };
 
-int v,e,k;
-const string inf_str = "INF";
-const int inf = 2e9;
-vector<edge> graph[20001];
-int dist[20001];
+const int sz = 2e4 + 1;
+const int inf = 1e9;
+int v, e, k;
+int dist[sz];
+vector<edge> graph[sz];
 
-void input() {
-    cin>>v>>e;
-    cin>>k;
-    int u,v,w;
-    for(int i=0; i<e; i++) {
-        cin>>u>>v>>w;
-        graph[u].push_back({v,w});
-    }
+void init() {
+	fill(dist, dist + sz, inf);
 }
 
-void solution() {
-    fill(dist,dist+20001,inf);
+void input() {
+	cin >> v >> e;
+	cin >> k;
+	int a, b, w;
+	while (e--) {
+		cin >> a >> b >> w;
+		graph[a].push_back({ b,w });
+	}
+}
 
-    priority_queue<edge> pq;
-    pq.push({k,0});
-    dist[k]=0;
+void dijkstra() {
+	priority_queue<edge> pq;
+	pq.push({ k,0 });
+	dist[k] = 0;
 
-    while(!pq.empty()) {
-        edge top = pq.top();
-        pq.pop();
+	while (!pq.empty()) {
+		edge top = pq.top();
+		pq.pop();
 
-        int now_vertex = top.vertex;
-        int now_weight = top.weight;
+		int vertex = top.vertex;
+		int weight = top.weight;
 
-        if(dist[now_vertex] < now_weight) {
-            continue;
-        }
+		if (dist[vertex] < weight) {
+			continue;
+		}
 
-        int length = graph[now_vertex].size();
-        for(int i=0; i<length; i++) {
-            edge next = graph[now_vertex][i];
+		for (auto next : graph[vertex]) {
+			if (dist[next.vertex] > weight + next.weight) {
+				dist[next.vertex] = weight + next.weight;
+				pq.push({ next.vertex,dist[next.vertex] });
+			}
+		}
+	}
+}
 
-            if(dist[next.vertex] > now_weight + next.weight) {
-                dist[next.vertex] = now_weight + next.weight;
-                pq.push({next.vertex, dist[next.vertex]});
-            }
-        }
-    }
-
-    for(int i=1; i<=v; i++) {
-        if(dist[i]>=inf) {
-            cout << inf_str;
-        } else {
-            cout<<dist[i];
-        }
-        cout<<'\n';
-    }
+void print() {
+	for (int i = 1; i <= v; i++) {
+		cout << (dist[i] >= inf ? "INF" : to_string(dist[i])) << '\n';
+	}
 }
 
 void solve() {
-    input();
-    solution();
+	init();
+	input();
+	dijkstra();
+	print();
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    solve();
+	solve();
 }

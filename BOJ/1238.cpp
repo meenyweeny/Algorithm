461ac16e1136db396757b48034e1391e3b95a986
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-21 16:46:27 +0900

    [#1] 1238 - 파티

--- BOJ/1238.cpp
+++ BOJ/1238.cpp
@@ -1,106 +1,105 @@
-//
-// Created by 이경민 on 2023/01/26.
-//
 #include<iostream>
-#include<vector>
 #include<queue>
+#include<vector>
 using namespace std;
 
 struct edge {
-    int vertex;
-    int weight;
+	int vertex;
+	int weight;
 
-    bool operator<(const edge& b) const {
-        return weight < b.weight;
-    }
+	bool operator<(const edge & b) const {
+		return weight > b.weight;
+	}
 };
 
-int n,m,x;
-int dist[1001];
-int revdist[1001];
-const int inf = 1e9;
-vector<edge> graph[1001];
-vector<edge> revgraph[1001];
+const int sz = 1e3 + 1;
+int n, m, x;
+vector<edge> graph[sz];
+vector<edge> revgraph[sz];
+int dist[sz];
+int revdist[sz];
+
+void init() {
+	fill(dist, dist + sz, 2e9);
+	fill(revdist, revdist + sz, 2e9);
+}
 
 void input() {
-    cin>>n>>m>>x;
-    int a,b,t;
-    while(m--) {
-        cin>>a>>b>>t;
-        graph[a].push_back({b,t});
-        revgraph[b].push_back({a, t});
-    }
+	cin >> n >> m >> x;
+	int u, v, t;
+	while (m--) {
+		cin >> u >> v >> t;
+		graph[u].push_back({ v,t });
+		revgraph[v].push_back({ u,t });
+	}
 }
 
 void dijkstra() {
-    priority_queue<edge> pq;
-    pq.push({x,0});
-    dist[x] = 0;
+	priority_queue<edge> pq;
+	pq.push({ x,0 });
+	dist[x] = 0;
+	while (!pq.empty()) {
+		int front = pq.top().vertex;
+		int weight = pq.top().weight;
+		pq.pop();
 
-    while(!pq.empty()) {
-        edge top = pq.top();
-        pq.pop();
+		if (dist[front] < weight) {
+			continue;
+		}
 
-        if(top.weight > dist[top.vertex]) {
-            continue;
-        }
-
-        int length = graph[top.vertex].size();
-        for(int i=0; i<length; i++) {
-            edge next = graph[top.vertex][i];
-
-            if(dist[next.vertex] > top.weight + next.weight) {
-                dist[next.vertex] = top.weight + next.weight;
-                pq.push({next.vertex, dist[next.vertex]});
-            }
-        }
-    }
+		for (auto e : graph[front]) {
+			if (dist[e.vertex] > weight + e.weight) {
+				dist[e.vertex] = weight + e.weight;
+				pq.push({ e.vertex,dist[e.vertex] });
+			}
+		}
+	}
 }
 
 void revdijkstra() {
-    priority_queue<edge> pq;
-    pq.push({x,0});
-    revdist[x] = 0;
+	priority_queue<edge> pq;
+	pq.push({ x,0 });
+	revdist[x] = 0;
+	while (!pq.empty()) {
+		int front = pq.top().vertex;
+		int weight = pq.top().weight;
+		pq.pop();
 
-    while(!pq.empty()) {
-        edge top = pq.top();
-        pq.pop();
+		if (revdist[front] < weight) {
+			continue;
+		}
 
-        if(top.weight > revdist[top.vertex]) {
-            continue;
-        }
-
-        int length = revgraph[top.vertex].size();
-        for(int i=0; i<length; i++) {
-            edge next = revgraph[top.vertex][i];
-
-            if(revdist[next.vertex] > top.weight + next.weight) {
-                revdist[next.vertex] = top.weight + next.weight;
-                pq.push({next.vertex, revdist[next.vertex]});
-            }
-        }
-    }
+		for (auto e : revgraph[front]) {
+			if (revdist[e.vertex] > weight + e.weight) {
+				revdist[e.vertex] = weight + e.weight;
+				pq.push({ e.vertex,revdist[e.vertex] });
+			}
+		}
+	}
 }
 
-void solution() {
-    fill(dist, dist+n+1, inf);
-    fill(revdist, revdist + n + 1, inf);
-
-    dijkstra();
-    revdijkstra();
-
-    int maxdist = -1;
-    for(int i=1; i<=n; i++) {
-        maxdist = max(dist[i] + revdist[i], maxdist);
-    }
-    cout<<maxdist;
+void solve() {
+	input();
+	init();
+	dijkstra();
+	revdijkstra();
+	int maxdist;
+	maxdist = 0;
+	for (int i = 1; i <= n; i++) {
+		if (i == x) {
+			continue;
+		}
+		if (dist[i] + revdist[i] > maxdist) {
+			maxdist = dist[i] + revdist[i];
+		}
+	}
+	cout << maxdist;
 }
 
 int main() {
-    cin.tie(nullptr);
-    cout.tie(nullptr);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    input();
-    solution();
+	solve();
 }

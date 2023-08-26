c2fc8b83325aa258409854bb2bc8b3c21b125c81
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-26 19:01:16 +0900

    [#1] 14888 - 연산자 끼워넣기

--- BOJ/14888.cpp
+++ BOJ/14888.cpp
@@ -1,59 +1,47 @@
 #include<iostream>
 using namespace std;
 
-int mini,maxi;
+int n, ansmin, ansmax;
+int arr[11];
 int oper[4];
-int nums[12];
-int n;
 
 void input() {
     cin>>n;
     for(int i=0; i<n; i++) {
-        cin>>nums[i];
+        cin>>arr[i];
     }
     for(int i=0; i<4; i++) {
         cin>>oper[i];
     }
 }
 
-int calc(int a,int b,int op) {
-    if(op==0) {
-        return a+b;
-    } else if(op==1) {
-        return a-b;
-    } else if(op==2) {
-        return a*b;
-    } else {
-        return a/b;
-    }
-}
-
-void run(int op[],int count,int sum) {
-    if(count==n) {
-        maxi = max(maxi,sum);
-        mini = min(mini,sum);
+void solution(int sum, int start) {
+    if(start==n) {
+        ansmax = max(ansmax,sum);
+        ansmin = min(ansmin,sum);
         return;
     }
-
     for(int i=0; i<4; i++) {
-        if(op[i]!=0) {
-            --op[i];
-            run(op,count+1,calc(sum,nums[count],i));
-            ++op[i];
+        if(!oper[i]) {
+            continue;
         }
+        --oper[i];
+        int tmp = sum;
+        if(i==0) tmp += arr[start];
+        else if(i==1) tmp -= arr[start];
+        else if(i==2) tmp *= arr[start];
+        else tmp /= arr[start];
+        solution(tmp,start+1);
+        ++oper[i];
     }
-}
-
-void solution() {
-    mini = 2e9;
-    maxi = -2e9;
-    run(oper,1,nums[0]);
-    cout<<maxi<<'\n'<<mini;
 }
 
 void solve() {
     input();
-    solution();
+    ansmin = 2e9;
+    ansmax = -2e9;
+    solution(arr[0],1);
+    cout<<ansmax<<'\n'<<ansmin;
 }
 
 int main() {

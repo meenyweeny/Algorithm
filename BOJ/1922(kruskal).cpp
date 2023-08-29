e856dafacd23946bde84261e63f709b88ebbd63f
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-29 10:39:47 +0900

    [#1] 1922(kruskal) - 네트워크 연결

--- BOJ/1922(kruskal).cpp
+++ BOJ/1922(kruskal).cpp
@@ -1,91 +1,83 @@
-//
-// Created by 이경민 on 2023/01/16.
-//
 #include<iostream>
-#include<vector>
+#include<deque>
 #include<algorithm>
 using namespace std;
 
 struct edge {
-    int start;
-    int end;
-    int weight;
+	int u, v, d;
+
+	bool operator<(const edge & b) const {
+		return d < b.d;
+	}
 };
 
-vector<edge> edges;
-int n,m,a,b,c;
-int parent[1001];
+const int sz = 1e3 + 1;
+int n, m;
+int par[sz];
+deque<edge> edges;
 
-bool comp(edge x, edge y) {
-    return x.weight < y.weight;
+void input() {
+	cin >> n >> m;
+	int a, b, c;
+	while (m--) {
+		cin >> a >> b >> c;
+		edges.push_back({ a,b,c });
+	}
 }
 
 void init() {
-    for(int i=1; i<=n; i++) { //union find 초기화
-        parent[i] = i;
-    }
+	for (int i = 1; i <= n; i++) {
+		par[i] = i;
+	}
 }
 
 int find(int x) {
-    if(parent[x] == x) {
-        return x;
-    } else {
-        parent[x] = find(parent[x]);
-        int result = parent[x];
-        return result;
-    }
+	if (x == par[x]) {
+		return x;
+	} else {
+		return par[x] = find(par[x]);
+	}
 }
 
-bool merge(int x,int y) {
-    int x_root = find(x);
-    int y_root = find(y);
-
-    if(x_root == y_root) {
-        return true;
-    } else {
-        parent[x_root] = y_root;
-        return false;
-    }
-}
-
-void input() {
-    cin>>n>>m;
-    while(m--) {
-        cin>>a>>b>>c;
-        edges.push_back({a,b,c});
-    }
+bool merge(int x, int y) {
+	int xroot = find(x);
+	int yroot = find(y);
+	if (xroot == yroot) {
+		return false;
+	} else {
+		if (xroot < yroot) {
+			par[yroot] = xroot;
+		} else {
+			par[xroot] = yroot;
+		}
+		return true;
+	}
 }
 
 void solution() {
-    sort(edges.begin(), edges.end(), comp);
+	sort(edges.begin(), edges.end());
+	int total = n - 1;
+	int weight = 0;
+	while (total > 0 && !edges.empty()) {
+		edge e = edges.front();
+		edges.pop_front();
+		if (merge(e.u, e.v)) {
+			weight += e.d;
+		}
+	}
+	cout << weight;
+}
 
-    init();
-
-    int mst_cost = 0;
-    int edge_count = 0;
-
-    int edge_size = edges.size();
-    edge tmp;
-    for(int i=0; i<edge_size; i++) {
-        tmp = edges[i];
-        if(!merge(tmp.start, tmp.end)) {
-            ++edge_count;
-            mst_cost += tmp.weight;
-        }
-
-        if(edge_count == n - 1) {
-            break;
-        }
-    }
-
-    cout << mst_cost;
+void solve() {
+	input();
+	init();
+	solution();
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    input();
-    solution();
+	solve();
 }

29910bb8600e6c8b59d79f5ec4739c96db73ecff
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-08-31 16:21:37 +0900

    [#1] 1197 - 최소 스패닝 트리

--- BOJ/1197.cpp
+++ BOJ/1197.cpp
@@ -1,65 +1,84 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 using namespace std;
 
-int v,e;
-int arr[10001];
-vector<pair<int,pair<int,int>>> edges;
+struct edge {
+	int u, v, d;
 
-int find(int x) {
-    if(arr[x]==x) {
-        return x;
-    } else {
-        return arr[x] = find(arr[x]);
-    }
-}
+	bool operator<(const edge & b) const {
+		return d < b.d;
+	}
+};
 
-bool merge(int x,int y) {
-    int xroot = find(x);
-    int yroot = find(y);
-
-    if(xroot!=yroot) {
-        arr[xroot] = yroot;
-        return false;
-    } else {
-        return true;
-    }
-}
+const int sz = 1e5 + 1;
+int v, e;
+int par[sz];
+vector<edge> edges;
 
 void input() {
-    cin>>v>>e;
-    int a,b,c;
-    while(e--) {
-        cin>>a>>b>>c;
-        edges.push_back({c,{a,b}});
-    }
+	cin >> v >> e;
+	int a, b, c;
+	while (e--) {
+		cin >> a >> b >> c;
+		edges.push_back({ a,b,c });
+	}
+}
+
+void init() {
+	for (int i = 0; i <= v; i++) {
+		par[i] = i;
+	}
+}
+
+int find(int x) {
+	if (par[x] == x) {
+		return x;
+	} else {
+		return par[x] = find(par[x]);
+	}
+}
+
+bool merge(int x, int y) {
+	int xroot = find(x);
+	int yroot = find(y);
+
+	if (xroot == yroot) {
+		return false;
+	}
+	if (xroot > yroot) {
+		swap(xroot, yroot);
+	}
+	par[xroot] = yroot;
+	return true;
 }
 
 void solution() {
-    for(int i=0; i<=v; i++) {
-        arr[i] = i;
-    }
-    int answer = 0;
-    sort(edges.begin(), edges.end());
-    for(auto edge:edges) {
-        if(merge(edge.second.first, edge.second.second)) {
-            continue;
-        } else {
-            answer += edge.first;
-        }
-    }
-    cout<<answer;
+	int goal = v - 1;
+	int cost = 0;
+	sort(edges.begin(), edges.end());
+	for (auto e : edges) {
+		if (merge(e.u, e.v)) {
+			cost += e.d;
+			--goal;
+		}
+		if (!goal) {
+			break;
+		}
+	}
+	cout << cost;
 }
 
 void solve() {
-    input();
-    solution();
+	input();
+	init();
+	solution();
 }
 
 int main() {
-    cin.tie(NULL);
-    cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
-    solve();
+	solve();
 }

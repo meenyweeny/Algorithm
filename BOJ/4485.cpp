88ecd0fc16a4bf4e8435693ea5fbd1fa80c8ac73
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-09-02 22:08:35 +0900

    [#1] 4485 - 녹색 옷 입은 애가 젤다지? 성공다국어

--- BOJ/4485.cpp
+++ BOJ/4485.cpp
@@ -1,79 +1,69 @@
-//
-// Created by 이경민 on 2023/02/13.
-//
 #include<iostream>
 #include<queue>
 using namespace std;
 
-struct point {
-    int x,y;
-    int weight;
+struct edge {
+    int x,y,weight;
 
-    bool operator<(const point & b) const {
+    bool operator<(const edge & b) const {
         return weight > b.weight;
     }
 };
 
-int map[126][126];
-int n;
-int dx[4] = {-1,0,1,0};
-int dy[4] = {0,-1,0,1};
-int dist[126][126];
 const int inf = 2e9;
+const int sz = 125;
+int n,answer;
+int map[sz][sz];
+int dist[sz][sz];
 
-bool is_in_range(int x,int y) {
-    return x>0 && x<=n && y>0 && y<=n;
+bool checkRange(int x,int y) {
+    return x>=0 && y>=0 && x<n && y<n;
 }
 
 void solution() {
-    fill(&dist[0][0], &dist[125][126], inf);
-    dist[1][1] = map[1][1];
-    priority_queue<point> pq;
-    pq.push({1,1, dist[1][1]});
+    fill(&dist[0][0],&dist[sz-1][sz],inf);
+    priority_queue<edge> pq;
+    dist[0][0] = map[0][0];
+    pq.push({0,0,dist[0][0]});
 
     while(!pq.empty()) {
-        point top = pq.top();
+        int fx = pq.top().x;
+        int fy = pq.top().y;
+        int weight = pq.top().weight;
         pq.pop();
 
-        if(dist[top.x][top.y] < top.weight) {
+        if(dist[fx][fy] < weight) {
             continue;
         }
-
         for(int i=0; i<4; i++) {
-            int nx = top.x + dx[i];
-            int ny = top.y + dy[i];
+            int nx = fx + "2011"[i] - '1';
+            int ny = fy + "1120"[i] - '1';
 
-            if(!is_in_range(nx,ny)) {
+            if(!checkRange(nx,ny)) {
                 continue;
             }
 
-            if(dist[nx][ny] > map[nx][ny] + top.weight) {
-                dist[nx][ny] = map[nx][ny] + top.weight;
+            if(dist[nx][ny]>weight + map[nx][ny]) {
+                dist[nx][ny] = weight + map[nx][ny];
                 pq.push({nx,ny,dist[nx][ny]});
             }
         }
     }
-}
-
-void input() {
-    cin>>n;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            cin>>map[i][j];
-        }
-    }
+    answer=dist[n-1][n-1];
 }
 
 void solve() {
-    int t=1;
-    while(1) {
-        input();
-        if(n==0) {
-            break;
+    int tc = 1;
+    cin>>n;
+    while(n) {
+        for(int i=0; i<n; i++) {
+            for(int j=0; j<n; j++) {
+                cin>>map[i][j];
+            }
         }
         solution();
-        cout<<"Problem "<<t<<": "<<dist[n][n]<<'\n';
-        ++t;
+        cout<<"Problem "<<tc++<<": "<<answer<<'\n';
+        cin>>n;
     }
 }
 

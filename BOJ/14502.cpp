e9154254899bb7e56dc1a0793be00c9764ec2010
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-09-25 12:14:49 +0900

    [#1] 14502 - 연구소

--- BOJ/14502.cpp
+++ BOJ/14502.cpp
@@ -1,45 +1,48 @@
 #include<iostream>
-#include<deque>
+#include<queue>
 #include<algorithm>
+#include<vector>
 using namespace std;
 
-const int bug = 2;
-const int wall = 1;
-int n, m, answer = -1;
+int n, m, answer;
 int map[8][8];
-deque<pair<int, int>> virus;
-deque<pair<int, int>> safearea;
-bool visited[8][8];
+int dup[8][8];
+queue<pair<int, int>> virus;
+vector<pair<int, int>> room;
 
-void init() {
+void input() {
+	cin >> n >> m;
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			visited[i][j] = (map[i][j] != 0);
+			cin >> map[i][j];
+			if (map[i][j] == 2) {
+				virus.push({ i,j });
+			} else if (map[i][j] == 0) {
+				room.push_back({ i,j });
+			}
 		}
 	}
 }
 
-int getSafeareaSize() {
-	int ret = 0;
+void copy() {
 	for (int i = 0; i < n; i++) {
 		for (int j = 0; j < m; j++) {
-			ret += (!visited[i][j]);
+			dup[i][j] = map[i][j];
 		}
 	}
-	return ret;
 }
 
 bool check_range(int x, int y) {
-	return x >= 0 && y >= 0 && x < n && y < m;
+	return x >= 0 && x < n && y >= 0 && y < m;
 }
 
-void bfs() {
-	deque<pair<int, int>> q = virus;
+int get_safe_area() {
+	queue<pair<int, int>> q = virus;
 	while (!q.empty()) {
 		int fx = q.front().first;
 		int fy = q.front().second;
-		q.pop_front();
-		visited[fx][fy] = true;
+		q.pop();
+
 		for (int i = 0; i < 4; i++) {
 			int nx = fx + "2011"[i] - '1';
 			int ny = fy + "1120"[i] - '1';
@@ -47,52 +50,48 @@ void bfs() {
 			if (!check_range(nx, ny)) {
 				continue;
 			}
-			if (visited[nx][ny]) {
+
+			if (dup[nx][ny] > 0) {
 				continue;
 			}
-			if (map[nx][ny] != 0) {
-				continue;
-			}
-			visited[nx][ny] = true;
-			q.push_back({ nx,ny });
+
+			dup[nx][ny] = 2;
+			q.push({ nx,ny });
 		}
+	}
+	int safe_area = 0;
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			safe_area += (dup[i][j] == 0);
+		}
+	}
+	return safe_area;
+}
+
+void process(int count, int start, int wall[3]) {
+	if (count == 3) {
+		copy();
+		for (int i = 0; i < 3; i++) {
+			dup[room[wall[i]].first][room[wall[i]].second] = 1;
+		}
+		answer = max(answer, get_safe_area());
+		return;
+	}
+	for (int i = start; i < room.size(); i++) {
+		wall[count] = i;
+		process(count + 1, i + 1, wall);
 	}
 }
 
-void run(int count,int x) {
-	if (count == 3) {
-		init();
-		bfs();
-		answer = max(answer, getSafeareaSize());
-		return;
-	}
-
-	for (int i = x; i < safearea.size(); i++) {
-		pair<int, int> now = safearea[i];
-		if (map[now.first][now.second] != 0) {
-			continue;
-		}
-		map[now.first][now.second] = wall;
-		run(count + 1, i + 1);
-		map[now.first][now.second] = 0;
-	}
+void solution() {
+	int list[3];
+	process(0, 0, list);
+	cout << answer;
 }
 
 void solve() {
-	cin >> n >> m;
-	for (int i = 0; i < n; i++) {
-		for (int j = 0; j < m; j++) {
-			cin >> map[i][j];
-			if (map[i][j] == bug) {
-				virus.push_back({i,j});
-			}
-			else if (!map[i][j]) {
-				safearea.push_back({ i,j });
-			}
-		}
-	}
-	run(0, 0);
-	cout << answer;
+	input();
+	solution();
 }
 
 int main() {

ef26991c23ba5cfc24dd6e0262e4ac38ae4ba584
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-10-02 16:28:10 +0900

    [#1] 2293 - 동전 1

--- BOJ/2293.cpp
+++ BOJ/2293.cpp
@@ -1,31 +1,37 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
-int n, k;
+int n,k;
 int arr[100];
 int dp[10001];
 
-void solve() {
-	cin >> n >> k;
-	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
+void input() {
+    cin>>n>>k;
+    for(int i=0; i<n; i++) {
+        cin>>arr[i];
+    }
+}
 
-	sort(arr, arr + n);
-	dp[0] = 1;
-	for (int i = 0; i < n; i++) {
-		for (int j = arr[i]; j <= k; j++) {
-			dp[j] += dp[j - arr[i]];
-		}
-	}
-	cout << dp[k];
+void solution() {
+    sort(arr,arr+n);
+    dp[0] = 1;
+    for(int i=0; i<n; i++) {
+        for(int j=arr[i]; j<=k; j++) {
+            dp[j] += dp[j-arr[i]];
+        }
+    }
+    cout<<dp[k];
+}
+
+void solve() {
+    input();
+    solution();
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-	solve();
+    solve();
 }

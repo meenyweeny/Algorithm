5668ef6f91ccb88e71e67a752c519eb4369bbee5
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-10-03 20:45:27 +0900

    [#1] 1932 - 정수 삼각형

--- BOJ/1932.cpp
+++ BOJ/1932.cpp
@@ -1,50 +1,33 @@
 #include<iostream>
-#include<algorithm>
 using namespace std;
 
 int n;
-int tri[501][501];
-int dp[501][501];
-
-void input() {
-	cin >> n;
-	for (int i = 1; i <= n; i++) {
-		for (int j = 1; j <= i; j++) {
-			cin >> tri[i][j];
-		}
-	}
-}
-
-void solution() {
-	dp[1][1] = tri[1][1];
-	dp[2][1] = (tri[2][1] + dp[1][1]);
-	dp[2][2] = (tri[2][2] + dp[1][1]);
-	
-	for (int i = 3; i <= n; i++) {
-		dp[i][1] = (tri[i][1] + dp[i - 1][1]);
-		for (int j = 2; j < i; j++) {
-			dp[i][j] += tri[i][j];
-			dp[i][j] += max(dp[i-1][j - 1], dp[i-1][j]);
-		}
-		dp[i][i] = (tri[i][i] + dp[i - 1][i - 1]);
-	}
-
-	int answer = -1;
-	for (int i = 0; i <= n; i++) {
-		answer = max(answer, dp[n][i]);
-	}
-	cout << answer;
-}
+int arr[500][500];
 
 void solve() {
-	input();
-	solution();
+    cin>>n;
+    cin>>arr[0][0];
+    for(int i=1; i<n; i++) {
+        for(int j=0; j<=i; j++) {
+            cin>>arr[i][j];
+            if(j==0) {
+                arr[i][j] += arr[i-1][j];
+            } else {
+                arr[i][j] += max(arr[i-1][j-1], arr[i-1][j]);
+            }
+        }
+    }
+    int answer = 0;
+    for(int i=0; i<n; i++) {
+        answer = max(answer,arr[n-1][i]);
+    }
+    cout<<answer;
 }
 
 int main() {
-	ios_base::sync_with_stdio(false);
-	cin.tie(NULL);
-	cout.tie(NULL);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    ios_base::sync_with_stdio(false);
 
-	solve();
+    solve();
 }

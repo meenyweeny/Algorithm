fa9c795c48311a1cf6af9a8ecdc26c850daf209d
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-12-05 14:31:56 +0900

    1288 - 새로운 불면증 치료법

--- SWEA/1288.cpp
+++ SWEA/1288.cpp
@@ -1,39 +1,48 @@
 #include<iostream>
 using namespace std;
-
-int n, k;
-int full = (1<<10) - 1;
-int check = 0;
-
-void init() {
-    k = 1;
-    check = 0;
-}
-
-bool solution() {
-    unsigned long long now = n*(k++);
-    while(now>0) {
-        check |= (1<<(now%10));
-        now/=10;
+ 
+int answer;
+unsigned long long num;
+bool visited[10];
+ 
+bool judge() {
+    for(int i=0; i<10; i++) {
+        if(!visited[i]) {
+            return false;
+        }
     }
-    return check!=full;
+    return true;
 }
-
+ 
+void solution() {
+    int count = 1;
+    fill(visited,visited+10,false);
+    while(!judge()) {
+        string s = to_string(num*count);
+        for(auto c:s) {
+            visited[c-'0'] = true;
+        }
+        ++count;
+    }
+    --count;
+    answer = count * num;
+}
+ 
 void solve() {
     int t;
     cin>>t;
     for(int tc=1; tc<=t; tc++) {
-        init();
-        cin>>n;
-        while(solution());
-        cout<<'#'<<tc<<' '<<n*(k-1)<<'\n';
+        cin>>num;
+        solution();
+        cout<<'#'<<tc<<' '<<answer<<'\n';
     }
 }
-
+ 
 int main() {
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
-
+ 
     solve();
+    return 0;
 }

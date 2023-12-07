1e1829c36486fe5d665f52155268dfd158e82336
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-12-07 15:41:52 +0900

    [#6] 23290 - 마법사 상어와 복제

--- 삼성코테/23290.cpp
+++ 삼성코테/23290.cpp
@@ -1,20 +1,18 @@
 #include<iostream>
-#include<deque>
 #include<vector>
 using namespace std;
 
 int m,s;
-deque<int> map[4][4];
-deque<int> copied[4][4];
-deque<int> tmp[4][4];
-vector<int> sharkdir;
-int eaten;
+int map[4][4][8]; //(x,y)에 방향 z인 물고기 몇마리인지
 int smell[4][4];
-int fdx[8] = {0,-1,-1,-1,0,1,1,1};
-int fdy[8] = {-1,-1,0,1,1,1,0,-1};
-int sdx[4] = {-1,0,1,0};
-int sdy[4] = {0,-1,0,1};
+int copied[4][4][8];
+int fx[8] = {0,-1,-1,-1,0,1,1,1};
+int fy[8] = {-1,-1,0,1,1,1,0,-1};
 pair<int,int> shark;
+int shx[4] = {-1,0,1,0};
+int shy[4] = {0,-1,0,1};
+vector<int> shark_path;
+int shark_eaten_fish;
 
 void input() {
     cin>>m>>s;
@@ -24,127 +22,41 @@ void input() {
         --x;
         --y;
         --d;
-        map[x][y].push_back(d);
+        ++map[x][y][d];
     }
     cin>>shark.first>>shark.second;
     --shark.first;
     --shark.second;
 }
 
-void copy_magic() {
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
-            copied[i][j] = map[i][j];
-        }
-    }
-}
-
 bool check_range(int x,int y) {
     return x>=0 && x<4 && y>=0 && y<4;
 }
 
-int turn_fish(int dir) {
-    return (dir==0) ? 7 : (dir-1);
-}
-
-void move_fish() {
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
-            tmp[i][j].clear();
-        }
-    }
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
-            for(int k=0; k<map[i][j].size(); k++) {
-                int nd = map[i][j][k];
-                int nx,ny;
-                while(1) {
-                    nx = i + fdx[nd];
-                    ny = j + fdy[nd];
-                    if(!check_range(nx,ny)) {
-                        nd = turn_fish(nd);
-                    } else if(nx==shark.first && ny==shark.second) {
-                        nd = turn_fish(nd);
-                    } else if(smell[nx][ny]>0) {
-                        nd = turn_fish(nd);
-                    } else {
-                        break;
-                    }
-                    if(nd==map[i][j][k]) {
-                        nx = i;
-                        ny = j;
-                        break;
-                    }
-                }
-                tmp[nx][ny].push_back(nd);
+void copy_fishes() {
+    for(int k=0; k<8; k++) {
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                copied[i][j][k] = map[i][j][k];
             }
-        }
-    }
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
-            map[i][j] = tmp[i][j];
         }
     }
 }
 
-void move_shark(vector<int> dirs) {
-    if(dirs.size()==3) {
-        //조합을 다 만들었으니, 상어를 진짜 움직일 수 있는 방향을 구한다
-        bool visited[4][4];
-        fill(&visited[0][0],&visited[3][4],false);
-        int nx = shark.first;
-        int ny = shark.second;
-        bool flag = true;
-        int eat = 0;
-        for(int i=0; i<3; i++) {
-            nx += sdx[dirs[i]];
-            ny += sdy[dirs[i]];
-            if(!check_range(nx,ny)) {
-                flag = false;
-                break;
-            }
-            if(!visited[nx][ny]) {
-                eat += map[nx][ny].size();
-                visited[nx][ny] = true;
+void apply_fishes() {
+    for(int k=0; k<8; k++) {
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                map[i][j][k] += copied[i][j][k];
             }
         }
-        if(flag) {
-            if(eaten<eat) {
-                eaten = eat;
-                sharkdir = dirs;
-            }
-        }
-        return;
-    }
-
-    for(int i=0; i<4; i++) {
-        dirs.push_back(i);
-        move_shark(dirs);
-        dirs.pop_back();
     }
 }
 
-void move_real_shark() {
-    int nx = shark.first;
-    int ny = shark.second;
-    for(int i=0; i<3; i++) {
-        nx += sdx[sharkdir[i]];
-        ny += sdy[sharkdir[i]];
-        if(map[nx][ny].empty()) {
-            continue;
-        } else {
-            map[nx][ny].clear();
-            smell[nx][ny] = 3;
-        }
-    }
-    shark = {nx,ny};
-}
-
-
-void adjust_smell() {
+void minus_smell() {
     for(int i=0; i<4; i++) {
         for(int j=0; j<4; j++) {
-            if(smell[i][j]==0) {
+            if(!smell[i][j]) {
                 continue;
             }
             --smell[i][j];
@@ -152,51 +64,142 @@ void adjust_smell() {
     }
 }
 
-void apply_copied() {
-    for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
-            for(int k=0; k<copied[i][j].size(); k++) {
-                map[i][j].push_back(copied[i][j][k]);
+int change_dir(int d) {
+    return d==0 ? 7 : d-1;
+}
+
+void move_fishes() {
+    int moved[4][4][8];
+    for(int k=0; k<8; k++) {
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                moved[i][j][k] = 0;
+            }
+        }
+    }
+    for(int k=0; k<8; k++) {
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                if(!map[i][j][k]) {
+                    continue;
+                }
+                int dir = k;
+                int fishes = map[i][j][k];
+                bool flag = false;
+                for(int iter=0; iter<8; iter++) {
+                    int nx = i + fx[dir];
+                    int ny = j + fy[dir];
+                    //불가능 -> dir 반시계방향으로 이동 후 continue
+                    if(!check_range(nx,ny)) {
+                        dir = change_dir(dir);
+                        continue;
+                    } else if(nx == shark.first && ny == shark.second) {
+                        dir = change_dir(dir);
+                        continue;
+                    } else if(smell[nx][ny]>0) {
+                        dir = change_dir(dir);
+                        continue;
+                    }
+                    //가능
+                    moved[nx][ny][dir] += fishes;
+                    flag = true;
+                    break;
+                }
+
+                if(!flag) {
+                    moved[i][j][k] += fishes;
+                }
+            }
+        }
+    }
+    //물고기를 다 움직였으면, moved가 map 자체가 된다
+    for(int k=0; k<8; k++) {
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                map[i][j][k] = moved[i][j][k];
             }
         }
     }
 }
 
-int get_fish_count() {
-    int ret = 0;
+void find_shark_path(vector<int> directions, int eaten_fishes, int bx, int by, int visited[4][4]) {
+    if(directions.size()==3) {
+        //먹은 fishes랑 비교
+        if(shark_eaten_fish<eaten_fishes) {
+            shark_eaten_fish = eaten_fishes;
+            shark_path = directions;
+        }
+        return;
+    }
+
     for(int i=0; i<4; i++) {
-        for(int j=0; j<4; j++) {
-            ret += map[i][j].size();
+        int nx = bx + shx[i];
+        int ny = by + shy[i];
+        if(!check_range(nx,ny)) {
+            continue;
+        }
+        directions.push_back(i);
+        int fish_count = 0;
+        if(!visited[nx][ny]) {
+            for(int k=0; k<8; k++) {
+                fish_count += map[nx][ny][k];
+            }
+        }
+        ++visited[nx][ny];
+        find_shark_path(directions,eaten_fishes+fish_count,nx,ny,visited);
+        --visited[nx][ny];
+        directions.pop_back();
+    }
+}
+
+void out_fish_remain_smell() {
+    for(auto sp:shark_path) {
+        shark.first += shx[sp];
+        shark.second += shy[sp];
+        int count = 0;
+        for(int k=0; k<8; k++) {
+            count += map[shark.first][shark.second][k];
+            map[shark.first][shark.second][k] = 0;
+        }
+        if(count) {
+            smell[shark.first][shark.second] = 3;
         }
     }
-    return ret;
+}
+
+void move_shark() {
+    shark_path.clear();
+    shark_eaten_fish = -1;
+    int visit[4][4];
+    fill(&visit[0][0], &visit[3][4], 0);
+    find_shark_path({},0,shark.first,shark.second,visit); //상어가 이동할 방향 3개 순서대로 찾기
+    out_fish_remain_smell(); //물고기를 제외시키고 냄새를 남긴다
 }
 
 void solve() {
     input();
-    for(int i=0; i<s; i++) {
-        //1. 물고기 복제 마법
-        copy_magic();
-        //2. 모든 물고기가 한 칸 이동
-        move_fish();
-        //3. 상어 연속 3칸 이동
-        eaten = -1;
-        sharkdir.clear();
-        vector<int> a;
-        move_shark(a);
-        move_real_shark();
-        //4. 두번 전 이동의 냄새 사라짐
-        adjust_smell();
-        //5. 복제 마법 끝나서 실제 물고기 적용
-        apply_copied();
+    while(s--) {
+        copy_fishes(); //상어가 복제 마법을 시전
+        move_fishes(); //모든 물고기가 한 칸 이동
+        move_shark(); //상어가 연속해서 3칸 이동
+        minus_smell(); //냄새 감소
+        apply_fishes(); //복제 마법이 적용
     }
-    cout<<get_fish_count();
+    int answer = 0;
+    for(int k=0; k<8; k++) {
+        for(int i=0; i<4; i++) {
+            for(int j=0; j<4; j++) {
+                answer += map[i][j][k];
+            }
+        }
+    }
+    cout<<answer;
 }
 
 int main() {
+    ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    ios_base::sync_with_stdio(false);
 
     solve();
     return 0;

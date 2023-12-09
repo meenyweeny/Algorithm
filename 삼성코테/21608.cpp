50013d31c04b0ced2addae7c59a111fd4e771452
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-12-09 21:55:27 +0900

    [#6] 21608 - 상어 초등학교

--- 삼성코테/21608.cpp
+++ 삼성코테/21608.cpp
@@ -1,25 +1,27 @@
 #include<iostream>
+#include<algorithm>
 #include<vector>
 #include<cmath>
 using namespace std;
 
-struct SeatInfo {
-    int x,y,like,empty;
-};
-
-int n,s;
+int n;
+bool like[401][401];
 int map[20][20];
-bool like_info[401][401];
+int answer = 0;
+vector<int> seat_sequence;
 
-bool comp(SeatInfo a, SeatInfo b) {
-    if(a.like == b.like) {
-        if(a.empty == b.empty) {
-            return a.x == b.x ? a.y < b.y : a.x < b.x;
-        } else {
-            return a.empty > b.empty;
+void input() {
+    cin>>n;
+    int k,s;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin>>k;
+            seat_sequence.push_back(k);
+            for(int q=0; q<4; q++) {
+                cin>>s;
+                like[k][s] = true;
+            }
         }
-    } else {
-        return a.like > b.like;
     }
 }
 
@@ -27,74 +29,86 @@ bool check_range(int x,int y) {
     return x>=0 && x<n && y>=0 && y<n;
 }
 
-int get_satisfied_point() {
-    int ret = 0;
+void find_seat(int now) {
+    int x,y;
+    x=-1;
+    y=-1;
+    int like_max = 0;
+    int empty_max = 0;
     for(int i=0; i<n; i++) {
         for(int j=0; j<n; j++) {
-            int up = 0;
-            int now_student = map[i][j];
-            for(int k=0; k<4; k++) {
-                int nx = i + "2011"[k] - '1';
-                int ny = j + "1120"[k] - '1';
-                if(!check_range(nx,ny)) {
-                    continue;
-                }
-                if(like_info[now_student][map[nx][ny]]) {
-                    ++up;
-                }
-            }
-            if(up!=0) {
-                --up;
-                ret += pow(10,up);
-            }
-        }
-    }
-    return ret;
-}
-
-void make_seat() {
-    vector<SeatInfo> seats;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            if(map[i][j]) {
+            if(map[i][j]!=0) {
                 continue;
             }
-            int empty = 0;
-            int likes = 0;
-            for(int k=0; k<4; k++) {
-                int nx = i + "2011"[k] - '1';
-                int ny = j + "1120"[k] - '1';
+            if(x==-1 && y==-1) {
+                x=i;
+                y=j;
+            }
+            int like_count = 0;
+            int empty_count = 0;
+            for(int dir=0; dir<4; dir++) {
+                int nx = i + "2011"[dir] - '1';
+                int ny = j + "1120"[dir] - '1';
                 if(!check_range(nx,ny)) {
                     continue;
                 }
                 if(map[nx][ny]==0) {
-                    ++empty;
-                } else {
-                    likes += like_info[s][map[nx][ny]];
+                    ++empty_count;
+                } else if(like[now][map[nx][ny]]) {
+                    ++like_count;
                 }
             }
-            seats.push_back({i,j,likes,empty});
+            if(like_max<like_count) {
+                x = i;
+                y = j;
+                like_max = like_count;
+                empty_max = empty_count;
+            } else if(like_max==like_count) {
+                if(empty_max<empty_count) {
+                    empty_max = empty_count;
+                    x = i;
+                    y = j;
+                }
+            }
         }
     }
-    sort(seats.begin(), seats.end(), comp);
-    SeatInfo next = seats[0];
-    map[next.x][next.y] = s;
+    map[x][y] = now;
+}
+
+void solution() {
+    for(auto s:seat_sequence) {
+        find_seat(s);
+    }
+}
+
+void get_goodpoint() {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            int count = 0;
+            int student = map[i][j];
+            for(int dir=0; dir<4; dir++) {
+                int nx = i + "2011"[dir] - '1';
+                int ny = j + "1120"[dir] - '1';
+                if(!check_range(nx,ny)) {
+                    continue;
+                }
+                if(like[student][map[nx][ny]]) {
+                    ++count;
+                }
+            }
+            if(count!=0) {
+                --count;
+                answer += pow(10,count);
+            }
+        }
+    }
 }
 
 void solve() {
-    cin>>n;
-    int a;
-    for(int i=0; i<n; i++) {
-        for(int j=0; j<n; j++) {
-            cin>>s;
-            for(int k=0; k<4; k++) {
-                cin>>a;
-                like_info[s][a] = true;
-            }
-            make_seat();
-        }
-    }
-    cout<<get_satisfied_point();
+    input();
+    solution();
+    get_goodpoint();
+    cout<<answer;
 }
 
 int main() {
@@ -103,4 +117,5 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     solve();
+    return 0;
 }

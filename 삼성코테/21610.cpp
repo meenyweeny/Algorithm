737bf3eed8592d63493acb50c3f5f1368fa37c84
Kyoungmin Lee <74659491+meenyweeny@users.noreply.github.com>
2023-12-09 21:08:19 +0900

    [#6] 21610 - 마법사 상어와 비바라기

--- 삼성코테/21610.cpp
+++ 삼성코테/21610.cpp
@@ -2,39 +2,41 @@
 #include<vector>
 using namespace std;
 
-struct Point {
-    int x,y;
-};
-
 int n,m,d,s;
-int map[51][51];
-vector<Point> cloud;
-vector<Point> water;
+int map[50][50];
+bool disappeared[50][50];
 int dx[8] = {0,-1,-1,-1,0,1,1,1};
 int dy[8] = {-1,-1,0,1,1,1,0,-1};
-bool visited[51][51];
+int cx[4] = {-1,-1,1,1};
+int cy[4] = {-1,1,-1,1};
+vector<pair<int,int>> cloud;
 
 void input() {
     cin>>n>>m;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            cin >> map[i][j];
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            cin>>map[i][j];
         }
     }
 }
 
-int get_next_pos(int dir,int x) {
-    x += (dir*s);
-    if(x>0) {
+bool check_range(int x,int y) {
+    return x>=0 && x<n && y>=0 && y<n;
+}
+
+int get_position(int x) {
+    if(x>=0 && x<n) {
+        return x;
+    } else if(x>=n) {
         while(1) {
-            if(x<=n) {
+            if(x>=0 && x<n) {
                 break;
             }
             x -= n;
         }
     } else {
         while(1) {
-            if(x>0) {
+            if(x>=0 && x<n) {
                 break;
             }
             x += n;
@@ -44,80 +46,84 @@ int get_next_pos(int dir,int x) {
 }
 
 void move_cloud() {
-    for(int i=0; i<cloud.size(); i++) {
-        cloud[i].x = get_next_pos(dx[d],cloud[i].x);
-        cloud[i].y = get_next_pos(dy[d],cloud[i].y);
-        ++map[cloud[i].x][cloud[i].y];
+    int size = cloud.size();
+    for(int i=0; i<size; i++) {
+        cloud[i] = {get_position(cloud[i].first + (dx[d] * s)),
+                    get_position(cloud[i].second + (dy[d] * s))};
     }
 }
 
-bool check_range(int x,int y) {
-    return x>0 && x<=n && y>0 && y<=n;
+void cloud_rain() {
+    for(auto c:cloud) {
+        ++map[c.first][c.second];
+    }
 }
 
-void copy_water() {
-    vector<int> v;
-    for(auto w:water) {
-        int count = 0;
+void disappear_cloud() {
+    fill(&disappeared[0][0], &disappeared[49][50], false);
+    for(auto c:cloud) {
+        disappeared[c.first][c.second] = true;
+    }
+    cloud.clear();
+}
+
+void copy_magic() {
+    int copymap[50][50];
+    fill(&copymap[0][0],&copymap[49][50],0);
+    for(auto c:cloud) {
         for(int i=0; i<4; i++) {
-            int nx = w.x + "0022"[i] - '1';
-            int ny = w.y + "0202"[i] - '1';
+            int nx = c.first + cx[i];
+            int ny = c.second + cy[i];
             if(!check_range(nx,ny)) {
                 continue;
             }
-            count += (map[nx][ny] != 0);
-        }
-        v.push_back(count);
-    }
-    fill(&visited[0][0],&visited[50][51],false);
-    for(int i=0; i<water.size(); i++) {
-        map[water[i].x][water[i].y] += v[i];
-        visited[water[i].x][water[i].y] = true;
-    }
-}
-
-void make_new_cloud() {
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            if(map[i][j]>=2 && !visited[i][j]) {
-                cloud.push_back({i,j});
-                map[i][j] -= 2;
+            if(map[nx][ny]!=0) {
+                ++copymap[c.first][c.second];
             }
         }
     }
-    water.clear();
-}
-
-void routine() {
-    move_cloud();
-    water = cloud;
-    cloud.clear();
-    copy_water();
-    make_new_cloud();
-}
-
-void solution() {
-    cloud.push_back({n-1,1}); //초기 구름
-    cloud.push_back({n-1,2});
-    cloud.push_back({n,1});
-    cloud.push_back({n,2});
-    while(m--) {
-        cin>>d>>s;
-        --d;
-        routine();
-    }
-    int answer = 0;
-    for(int i=1; i<=n; i++) {
-        for(int j=1; j<=n; j++) {
-            answer += map[i][j];
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            map[i][j] += copymap[i][j];
         }
     }
-    cout<<answer;
+}
+
+void make_cloud() {
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            if(map[i][j]>=2) {
+                if(!disappeared[i][j]) {
+                    map[i][j]-=2;
+                    cloud.push_back({i,j});
+                }
+            }
+        }
+    }
 }
 
 void solve() {
     input();
-    solution();
+    cloud.push_back({n-2,0});
+    cloud.push_back({n-2,1});
+    cloud.push_back({n-1,0});
+    cloud.push_back({n-1,1});
+    while(m--) {
+        cin>>d>>s;
+        --d;
+        move_cloud(); //모든 구름이 d방향으로 s만큼 이동
+        cloud_rain(); //구름에서 비가 내림
+        copy_magic(); //물복사버그
+        disappear_cloud(); //구름이 모두 사라짐
+        make_cloud(); //물의 양이 2이상인 모든 칸에 구름이 생김
+    }
+    int answer = 0;
+    for(int i=0; i<n; i++) {
+        for(int j=0; j<n; j++) {
+            answer += map[i][j];
+        }
+    }
+    cout<<answer;
 }
 
 int main() {
@@ -126,4 +132,5 @@ int main() {
     ios_base::sync_with_stdio(false);
 
     solve();
+    return 0;
 }
